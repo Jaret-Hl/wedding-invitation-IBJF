deb46ff4e3354e2b42d4ace3f958a6fbb4e5b808
Jaret-Hl <gadir041499@gmail.com>
2025-07-09 16:12:58 -0600

    feat: mejorar el diseño de la página de invitación con nuevas fuentes y estilos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,9 +14,9 @@ export default function Hero() {
 
   return (
     <section className="py-10 text-center px-4 sm:px-6 lg:px-10 max-w-3xl mx-auto">
-      <h1 className="hero-text text-5xl font-bold italic">¡Nos Casamos!</h1>
-      <h2 className="hero-text text-4xl mt-4">Isis Bethania y José Francisco</h2>
-      <p className="hero-text text-xl mt-4 italic">
+      <h1 className="hero-text text-4xl font-bold font-dancing-script mb-4">¡Nos Casamos!</h1>
+      <h2 className="hero-text text-5xl mt-4 font-dancing-script text-[#9e7043]">Isis Bethania y José Francisco</h2>
+      <p className="hero-text text-xl mt-4 bentham-regular">
         "El amor todo lo puede..." (1 Juan 4:16)
       </p>
     </section>
